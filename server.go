4f234425843e20c61291003b32541194af7f369f
spooks <g4briel.4lves@gmail.com>
2018-09-24 08:43:31 -0300

    atualiza build

--- server.go
+++ server.go
@@ -4,6 +4,7 @@ import (
         "net"
         "log"
         "fmt"
+        "strings"
 )
 
 func handleConn(c net.Conn) {
@@ -13,18 +14,29 @@ func handleConn(c net.Conn) {
 
         for {
                 length, err := c.Read(msg)
+                smsg := strings.Replace(string(msg), "\x00", "", -1)
                 if err != nil {
                         log.Println("Got", err, "from", c.RemoteAddr())
                         c.Close()
                         break
                 }
                 if length == 1 && msg[0] == '\n' {
-                        fmt.Println(c.RemoteAddr(), "is quitting...")
-                        c.Close()
-                        break
+                        fmt.Fprintf(c, "Would you like to quit? [y/n]\n")
+                        _, err := c.Read(msg)
+                        if msg[0] == 'y' {
+                            fmt.Println(c.RemoteAddr(), "is quitting...")
+                            c.Close()
+                            break
+                        }
+                        if err != nil {
+                            log.Println("Got", err, "from", c.RemoteAddr())
+                            c.Close()
+                            break
+                        }
                 }
-                fmt.Println(c.RemoteAddr(),"says:", string(msg))
-                fmt.Fprintf(c, "Got %d bytes\n", length)
+                msg = []byte(smsg[:len(smsg)-1])
+                fmt.Println(c.RemoteAddr(),"says:", string(msg), ", message length", length, "bytes")
+                fmt.Fprintf(c,"%v says: %s", c.RemoteAddr(), string(msg))
         }
 }
 
